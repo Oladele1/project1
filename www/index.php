14ccea346f1e61dc0b0080ed548490e8791cd2a2
oladele1 <ajayioladele2016@gmail.com>
2019-05-04 15:32:47 +0000

    adding git

---
+++ www/index.php
@@ -0,0 +1,19 @@
+<?php
+echo "<br>";
+echo "<hr>";
+echo "Thanks for coming";
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Welcome</title>
+</head>
+<hr/>
+
+<body>
+<p><h1> Welcome to Olibobs page</h1></p>
+
+
+</body>
+</html>
